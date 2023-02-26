254f0ddd71da7ec50e52d2c76bb77502261345d7
Maksim Komkov <KomkovMS@yandex.ru>
2023-02-26 22:48:23 +0700

    Les 3 доп материал map() + Object.assign()

--- Les3/Metod_Arr/map.js
+++ Les3/Metod_Arr/map.js
@@ -11,3 +11,64 @@ const newNums = nums.map(function (value) {
 	return value + 10;
 });
 console.log(newNums);
+
+
+// ЗАДАЧА
+// Перед вами массив с пользователями, используя метод map() создайте новый
+// массив, в котором появится префикс к фамилии Mr. или Mrs. в зависимости
+// от пола пользователя, например для объекта:
+
+// {
+// 	surname: 'Campbell',
+// 	gender: 'male',
+// },
+// результат будет:
+// {
+// 	surname: 'Campbell',
+// 	gender: 'male',
+//  appreal: 'Mr.',
+// },
+
+
+const users = [
+	{
+		surname: 'Campbell',
+        gender:'male',
+	},
+	{
+		surname: 'Miller',
+        gender: 'female',
+	},
+	{
+		surname: 'Jones',
+        gender: 'female',
+	},
+	{
+        surname: 'Brown',
+        gender: 'female',
+    },
+	{
+		surname: 'Smith',
+        gender:'male',
+	},
+];
+
+const usersGender = users.map(function (user) {
+	if (user.gender === 'male') {
+		user.appreal = 'Mr.';
+	}
+	user.appreal = 'Mrs.';
+	return user;
+});
+
+console.log(usersGender);
+
+// Вариант решения с помощью Object.assign():
+const usersWithAppeal = users.map(function (user) {
+	if (user.gender === 'male') {
+		return Object.assign({}, user, {appreal: 'Mr.'});
+	}
+	return Object.assign({}, user, {appreal: 'Mrs.'});
+});
+
+console.log(usersWithAppeal);
